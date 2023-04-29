56d4051f3b3da69704b2d17cea34c13c1aae17cb
dasarpan0402 <130377623+dasarpan0402@users.noreply.github.com>
2023-04-29 11:43:10 +0530

    Introduction to Javascript

---
+++ Introduction_to_Javascript/Problem2.js
@@ -0,0 +1,11 @@
+let name = "Arpan Das";
+
+console.log(name);
+
+name = "Late Dilip Das";
+
+console.log(name);
+
+name = "Pratima Das";
+
+console.log(name);
---
+++ Introduction_to_Javascript/Problem3.js
@@ -0,0 +1,5 @@
+let name = "Arpan Das";
+let age = 25;
+
+console.log(name,age);
+console.log(typeof(name),typeof(age));
